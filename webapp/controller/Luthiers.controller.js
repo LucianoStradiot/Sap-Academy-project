a81a30c8734457fec48df428bef0e34ca451beb8
Luciano <lstradiot15@gmail.com>
2024-05-06 15:45:09 -0300

    Resolve navigation problem with singleselectmaster and multiselect

--- webapp/controller/Luthiers.controller.js
+++ webapp/controller/Luthiers.controller.js
@@ -11,12 +11,39 @@ sap.ui.define(
     return Controller.extend("aca20241q.controller.Luthiers", {
       onInit: function () {
         that = this;
+        var gridList = this.getView().byId("gridList");
+        var mode = gridList.getMode();
+        if (mode === "SingleSelectMaster") {
+          gridList.attachSelectionChange(this.onPressNavigation);
+        }
+        if (mode === "MultiSelect") {
+          gridList.attachSelectionChange(this.deleteSelected);
+        }
       },
 
       handleNav: function (oEvent) {
         var oRouter = this.getOwnerComponent().getRouter();
         var sKey = oEvent.getParameter("key");
         oRouter.navTo(sKey);
+      },
+
+      onModeChange: function (oEvent) {
+        var sMode = oEvent.getParameter("item").getKey();
+        var oButton = this.byId("idDeleteButton2");
+        var gridList = this.getView().byId("gridList");
+        gridList.setMode(sMode);
+
+        if (sMode === "SingleSelectMaster") {
+          gridList.attachSelectionChange(this.onPressNavigation);
+        }
+        if (sMode === "Delete") {
+          gridList.detachSelectionChange(this.deleteLuthier);
+        }
+        if (sMode === "MultiSelect") {
+          oButton.setVisible(true);
+        } else {
+          oButton.setVisible(false);
+        }
       },
 
       createLuthier: function (oEvent) {
@@ -73,6 +100,7 @@ sap.ui.define(
           },
         });
       },
+
       onCloseLuthierPress: function (oEvent) {
         this.oCreateFragment.then(
           function (oDialog) {
@@ -82,25 +110,11 @@ sap.ui.define(
         );
       },
 
-      deleteSelected: function () {
-        var gridList = this.getView().byId("gridList");
-        var selectedItems = gridList.getSelectedItems();
-
-        if (selectedItems.length > 0) {
-          var model = this.getView().getModel();
-          selectedItems.forEach(function (item) {
-            var context = item.getBindingContext();
-            var path = context.getPath();
-            model.remove(path);
-          });
-          gridList.removeSelections();
-        } else {
-          sap.m.MessageToast.show("Please select at least one item to delete.");
-        }
-      },
-
       deleteLuthier: function (oEvent) {
-        let sPath = oEvent.getSource().getBindingContext().getPath();
+        var sPath = oEvent
+          .getParameter("listItem")
+          .getBindingContext()
+          .getPath();
 
         sap.m.MessageBox.confirm(
           "¿Estás seguro de que quieres eliminar el Luthier?",
@@ -132,6 +146,47 @@ sap.ui.define(
         );
       },
 
+      deleteSelected: function () {
+        var gridList = this.getView().byId("gridList");
+        var selectedItems = gridList.getSelectedItems();
+
+        if (selectedItems.length > 0) {
+          var model = this.getView().getModel();
+          var promises = [];
+          selectedItems.forEach(function (item) {
+            var context = item.getBindingContext();
+            var path = context.getPath();
+            var promise = new Promise(function (resolve, reject) {
+              model.remove(path, {
+                success: function () {
+                  sap.m.MessageToast.show("Elemento eliminado correctamente");
+                  resolve();
+                },
+                error: function (oError) {
+                  sap.m.MessageToast.show(
+                    "Error al eliminar el elemento: " + JSON.stringify(oError)
+                  );
+                  reject(oError);
+                },
+              });
+            });
+            promises.push(promise);
+          });
+
+          Promise.all(promises)
+            .then(function () {
+              gridList.removeSelections();
+            })
+            .catch(function (error) {
+              console.error("Error al eliminar elementos:", error);
+            });
+        } else {
+          sap.m.MessageToast.show(
+            "Por favor seleccione al menos un elemento para eliminar."
+          );
+        }
+      },
+
       updateLuthier: function (oEvent) {
         // Obtener el índice del luthier seleccionado en la lista
         var sPath = oEvent.getSource().getBindingContext().getPath();
@@ -150,7 +205,6 @@ sap.ui.define(
           }).then(
             function (oDialog) {
               that.getView().addDependent(oDialog);
-              // Establecer el modelo con los datos del luthier seleccionado
               let oModel = new sap.ui.model.json.JSONModel(oLuthier);
               oModel.setDefaultBindingMode("TwoWay");
               oDialog.setModel(oModel, "UpdateLuthier");
@@ -159,8 +213,6 @@ sap.ui.define(
             }.bind(that)
           );
         }
-
-        // Abrir el fragmento de actualización
         this.oUpdateFragment.then(function (oDialog) {
           oDialog.open();
         });
@@ -190,25 +242,28 @@ sap.ui.define(
       },
 
       onCloseUpdateLuthierPress: function (oEvent) {
-        // Cerrar el fragmento de actualización
         this.oUpdateFragment.then(function (oDialog) {
           oDialog.close();
         });
       },
 
       _afterCloseUpdateDialog: function (oEvent) {
-        // Limpiar y destruir el fragmento de actualización
         oEvent.getSource().destroy();
         that.oUpdateFragment = null;
       },
 
       onPressNavigation: function (oEvent) {
-        var oListItem = oEvent.getParameter("listItem");
-        var sLuthier = oListItem.getBindingContext().getProperty("IdLuthier");
-        var router = that.getOwnerComponent().getRouter();
-        router.navTo("LuthiersDetail", {
-          idLuthier: sLuthier,
-        });
+        var oSegmentedButton = that.byId("_IDGenSegmentedButton2");
+        var sSelectedKey = oSegmentedButton.getSelectedKey();
+
+        if (sSelectedKey === "SingleSelectMaster") {
+          var oListItem = oEvent.getParameter("listItem");
+          var sLuthier = oListItem.getBindingContext().getProperty("IdLuthier");
+          var router = that.getOwnerComponent().getRouter();
+          router.navTo("LuthiersDetail", {
+            idLuthier: sLuthier,
+          });
+        }
       },
 
       onSearch: function (oEvent) {
@@ -258,33 +313,6 @@ sap.ui.define(
 
         this.getView().byId("gridList").getBinding("items").filter(aFilters);
       },
-
-      /* onModeChange: function (oEvent) {
-        var sMode = oEvent.getParameter("item").getKey();
-        this.byId("gridList").setMode(sMode);
-        this.byId("gridList").setHeaderText("GridList with mode " + sMode);
-      },
-
-      onSelectionChange: function (oEvent) {
-        var bSelected = oEvent.getParameter("selected");
-        MessageToast.show(
-          (bSelected ? "Selected" : "Unselected") +
-            " item with ID " +
-            oEvent.getParameter("listItem").getId()
-        );
-      },
-
-      onDelete: function (oEvent) {
-        MessageToast.show(
-          "Delete item with ID " + oEvent.getParameter("listItem").getId()
-        );
-      },
-
-      onDetailPress: function (oEvent) {
-        MessageToast.show(
-          "Request details for item with ID " + oEvent.getSource().getId()
-        );
-      }, */
     });
   }
 );
